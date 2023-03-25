7b4c32697b362ce9fdb3fdfe44295d35f6ca83f6
AnatoliySokolov-code <109688326+AnatoliySokolov-code@users.noreply.github.com>
2023-03-25 13:33:06 +0300

    создал файл с токеном и ключами

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = '6187649235:AAE5nEsCEcJnkb4-7_P44YoYzt3VfEbViCY'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'тенге': 'KZT',
+    'юань': 'CNY',
+}
